d258fa50124034658651aa9dba662b8707af70b9
Aditya <aditya@teebeds.com>
2023-08-10 23:59:23 +0530

    Ui changes and top nav bar added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,11 +14,176 @@ import "@styles/global.scss";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="h-100">
+      <header>
+        <nav>
+          <div>
+            <g transform="translate(-8.000000, -2.000000)">
+              <g transform="translate(11.000000, 5.000000)">
+                <polygon
+                  id="Shape"
+                  stroke="currentColor"
+                  stroke-width="5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  points="39 0 0 22 0 67 39 90 78 68 78 23"
+                ></polygon>
+                <path
+                  d="M45.691667,45.15 C48.591667,46.1 50.691667,48.95 50.691667,52.2 C50.691667,57.95 46.691667,61 40.291667,61 L28.541667,61 L28.541667,30.3 L39.291667,30.3 C45.691667,30.3 49.691667,33.15 49.691667,38.65 C49.691667,41.95 47.941667,44.35 45.691667,45.15 Z M33.591667,43.2 L39.241667,43.2 C42.791667,43.2 44.691667,41.85 44.691667,38.95 C44.691667,36.05 42.791667,34.8 39.241667,34.8 L33.591667,34.8 L33.591667,43.2 Z M33.591667,47.5 L33.591667,56.5 L40.191667,56.5 C43.691667,56.5 45.591667,54.75 45.591667,52 C45.591667,49.2 43.691667,47.5 40.191667,47.5 L33.591667,47.5 Z"
+                  fill="currentColor"
+                ></path>
+              </g>
+            </g>
+          </div>
+          <div>
+            <button className="button--primary fs-13px mt-0  px-3 py-1">
+              Resume
+            </button>
+          </div>
+        </nav>
+      </header>
       <main>
-        <div className="">
+        <div className="fillHeight">
           <Component {...pageProps} />
         </div>
+        <hr />
+        <section>
+          <footer className="pb-3">
+            <div className="footer-social-links center mb-5">
+              <ul className="center">
+                <li>
+                  <a
+                    href="https://github.com/adityaadhikari15"
+                    aria-label="GitHub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div>
+                      <img src="/assets/images/github.svg" width={20} />
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/adityaadhikari1993/"
+                    aria-label="Instagram"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div>
+                      <img src="/assets/images/instagram.svg" width={20} />
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/adityaadhikari15/"
+                    aria-label="Linkedin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src="/assets/images/linkedin.svg" width={20} />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.upwork.com/freelancers/adityaadhikari"
+                    aria-label="Codepen"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src="/assets/images/upwork.svg" width={20} />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="center text-center">
+              <div>
+                <div className="font-mono fs-12px">
+                  Designed & Built by Aditya Adhikari
+                </div>
+                <div className="font-mono fs-12px">
+                  This website is built using three important ingredients
+                </div>
+              </div>
+            </div>
+
+            <div className="center mt-5 mb-16px">
+              <div>
+                <div>
+                  <span className="footer-card text-black">
+                    <img src="/assets/images/nextjs.svg" width={20} /> Nextjs
+                  </span>
+                  &nbsp;
+                  <span className="footer-card text-black">
+                    <img src="/assets/images/sass.svg" width={20} /> Sass
+                  </span>
+                  &nbsp;
+                  <span className="footer-card text-black">❤️ Love</span>
+                </div>
+                <div className="text-center"> </div>
+              </div>
+            </div>
+          </footer>
+        </section>
       </main>
+
+      <div className="fixed-side-bar left">
+        <ul>
+          <li>
+            <a
+              href="https://github.com/adityaadhikari15"
+              aria-label="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div>
+                <img src="/assets/images/github.svg" width={20} />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/adityaadhikari1993/"
+              aria-label="Instagram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div>
+                <img src="/assets/images/instagram.svg" width={20} />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/adityaadhikari15/"
+              aria-label="Linkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/linkedin.svg" width={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.upwork.com/freelancers/adityaadhikari"
+              aria-label="Codepen"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/upwork.svg" width={20} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="fixed-side-bar right">
+        <ul>
+          <li>
+            <a href="mailto:adityaadhikari15@gmail.com">
+              adityaadhikari15@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,142 +1,161 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { MyProjects } from "@/components/my-projects/my-projects";
 import MainStyles from "./main.module.scss";
-const inter = Inter({ subsets: ["latin"] });
-
+import { MyPreviousWorks } from "@/components/my-previous-jobs/my-previous-jobs";
 export default function Home() {
+  const mailTo = () => {
+    window.location.href = "mailto:adityaadhikari15@gmail.com";
+  };
   return (
-    <main>
-      <div className="container">
-        {" "}
-        <div className={MainStyles.landing_page}>
-          <div className="hero_section">
-            <div className="row">
-              <div className="col-md-12 m-auto">
-                <div className="fw-bolder mb-24px text-primary font-mono">
-                  Hi, my name is
-                </div>
-                <h1 className="big-heading">Aditya Adhikari</h1>
-                <div>
-                  {" "}
-                  <h3 className="big-heading">I build things for the web.</h3>
-                </div>
-                <p>
-                  I’m a software engineer specializing in building (and
-                  occasionally designing) exceptional digital experiences.
-                  Currently, I’m focused on building accessible, human-centered
-                  products at Upstatement.
-                </p>
-                <div className="mt-32px">
-                  <button className="button--primary mr-24px">
-                    View my work
-                  </button>
-                  <button className="button--primary">Download Resume</button>
-                </div>
+    <div className="container">
+      <div className={MainStyles.landing_page}>
+        <div className="hero_section">
+          <div className="row">
+            <div className="col-md-12 m-auto">
+              <div className="fw-bolder mb-24px text-primary font-mono">
+                Hi, my name is
+              </div>
+              <h1 className="big-heading">Aditya Adhikari.</h1>
+              <div>
+                {" "}
+                <h3 className="big-heading">I build things for the web.</h3>
+              </div>
+              <p>
+                I’m a software engineer specializing in building (and
+                occasionally designing) exceptional digital experiences.
+                Currently, I’m focused on building accessible, human-centered
+                products at Upstatement.
+              </p>
+              <div className="mt-32px">
+                <button className="button--primary mr-24px">
+                  View my work
+                </button>
+                <button className="button--primary" onClick={mailTo}>
+                  Hire me
+                </button>
               </div>
             </div>
           </div>
-          <section>
-            <h2 className="numbered-heading">
-              <span>1.</span>About me
-            </h2>
-            <div className="row">
-              <div className="col-md-8">
-                <p>
-                  Hello! My name is Brittany and I enjoy creating things that
-                  live on the internet. My interest in web development started
-                  back in 2012 when I decided to try editing custom Tumblr
-                  themes — turns out hacking together a custom reblog button
-                  taught me a lot about HTML & CSS! Fast-forward to today, and
-                </p>
-                <p>
-                  I’ve had the privilege of working at an advertising agency, a
-                  start-up, a huge corporation, and a student-led design studio.
-                  My main focus these days is building accessible, inclusive
-                  products and digital experiences at Upstatement for a variety
-                  of clients.
-                </p>
-                <p>
-                  I also recently launched a course that covers everything you
-                  need to build a web app with the Spotify API using Node &
-                  React.
-                </p>
-              </div>
-              <div className="col-md-4 center">
-                <div className={MainStyles.personal_image_wrapper}>
-                  {/* <img
+        </div>
+        <section>
+          <h2 className="numbered-heading">
+            <span>01.</span>About me
+          </h2>
+          <div className="row">
+            <div className="col-md-8">
+              <p>
+                Hello! My name is Brittany and I enjoy creating things that live
+                on the internet. My interest in web development started back in
+                2012 when I decided to try editing custom Tumblr themes — turns
+                out hacking together a custom reblog button taught me a lot
+                about HTML & CSS! Fast-forward to today, and
+              </p>
+              <p>
+                I&apos;ve had the privilege of working at an advertising agency,
+                a start-up, a huge corporation, and a student-led design studio.
+                My main focus these days is building accessible, inclusive
+                products and digital experiences at Upstatement for a variety of
+                clients.
+              </p>
+              <p>
+                I also recently launched a course that covers everything you
+                need to build a web app with the Spotify API using Node & React.
+              </p>
+            </div>
+            <div className="col-md-4 center">
+              <div className={MainStyles.personal_image_wrapper}>
+                {/* <img
                     className="ml-32px"
                     src="https://v4.brittanychiang.com/static/30a645f7db6038f83287d0c6042d3b2b/f9526/me.avif"
                     alt=""
                   /> */}
-                </div>
               </div>
             </div>
-          </section>
-          <section>
-            <h2 className="numbered-heading">
-              <span>2.</span> Skills & Tools
-            </h2>
-            <div className={MainStyles.skills_list}>
-              <ul>
-                <li>
-                  <div>
-                    <img src="/assets/images/angular.svg" />
-                    <div className={MainStyles.skill_name}>Angular</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/react.svg" />
-                    <div className={MainStyles.skill_name}>React</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/nextjs.svg" />
-                    <div className={MainStyles.skill_name}>Nextjs</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/ts.svg" />
-                    <div className={MainStyles.skill_name}>Typescript</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/sass.svg" />
-                    <div className={MainStyles.skill_name}>Sass</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/rxjs.svg" />
-                    <div className={MainStyles.skill_name}>Rxjs</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/redux.svg" />
-                    <div className={MainStyles.skill_name}>Redux</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/javascript.svg" />
-                    <div className={MainStyles.skill_name}>Javascript</div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <img src="/assets/images/html.svg" />
-                    <div className={MainStyles.skill_name}>Html</div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </section>
-        </div>
+          </div>
+        </section>
+        <section>
+          <h2 className="numbered-heading">
+            <span>2.</span> Skills & Tools
+          </h2>
+          <div className={MainStyles.skills_list}>
+            <ul>
+              <li>
+                <div>
+                  <img src="/assets/images/angular.svg" />
+                  <div className={MainStyles.skill_name}>Angular</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/react.svg" />
+                  <div className={MainStyles.skill_name}>React</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/nextjs.svg" />
+                  <div className={MainStyles.skill_name}>Nextjs</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/ts.svg" />
+                  <div className={MainStyles.skill_name}>Typescript</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/sass.svg" />
+                  <div className={MainStyles.skill_name}>Sass</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/rxjs.svg" />
+                  <div className={MainStyles.skill_name}>Rxjs</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/redux.svg" />
+                  <div className={MainStyles.skill_name}>Redux</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/javascript.svg" />
+                  <div className={MainStyles.skill_name}>Javascript</div>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <img src="/assets/images/html.svg" />
+                  <div className={MainStyles.skill_name}>Html</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <MyPreviousWorks />
+        </section>
+        <section>
+          <MyProjects />
+        </section>
+        <hr />
+        <section>
+          <h2 className="text-white fw-bold">
+            In need of a frontend developer?
+          </h2>
+          <div className="fw-semibold text-primary fs-16px">
+            <span className="pulse"></span>&nbsp;&nbsp;I’m currently available
+            for work.
+          </div>
+          <div className="mt-32px">
+            <button className="button--primary mr-24px">Send me message</button>
+            <button className="button--primary">Download Resume</button>
+          </div>
+        </section>
       </div>
-    </main>
+    </div>
   );
 }
