439366bde904b6a8ae4d5c5a442d69c262678f54
Aditya Adhikari <adityaadhikari15@gmail.com>
2025-05-19 14:51:52 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
   const openResume = () => {
     window.open(
-      "https://adityaadhikari.in/Aditya_Adhikari_Resume_Upwork_Angular.pdf",
+      "https://adityaadhikari.in/resume.pdf",
       "_blank",
       "noopener, noreferrer"
     );
